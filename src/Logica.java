973e9078e386fcd0cc5ee934c203e01302c305df
Juan Manuel Cordoba <juanmaco-97@hotmail.com>
2016-09-15 08:42:11 -0500

    se crea la logica y se aplica el app de PApplet

---
+++ src/Logica.java
@@ -0,0 +1,9 @@
+import processing.core.PApplet;
+
+public class Logica {
+public PApplet app=Main.app;
+
+public Logica(PApplet app) {
+	this.app = app;
+}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import processing.core.PApplet;
 
-public class Main {
+public class Main extends PApplet {
+public static Logica log;
+public static PApplet app;
 
+public void setup(){
+	app=this;
+	size(1200,700);
+	log= new Logica(this);
+}
 }
