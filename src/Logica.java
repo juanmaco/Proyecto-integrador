cc07e1abdc7d65e2d31b7a4ba03c7e4cbde2667d
Daniel_Lince <josedaniellince@hotmail.com>
2016-09-15 08:40:43 -0500

    Se crearon los metodos para la clase Logica y se creo la clase Imagen

---
+++ src/Logica.java
@@ -0,0 +1,38 @@
+import processing.core.PApplet;
+
+public class Logica {
+public Logica(){
+	
+}
+private void cargarImagenes(){
+	
+}
+private void cargarLista(){
+	
+}
+private void organizarImagenes(){
+	
+}
+private void comprobarFile() {
+	// TODO Auto-generated method stub
+
+}
+
+private void zoomIn() {
+	// TODO Auto-generated method stub
+
+}
+private void zoomOut() {
+	// TODO Auto-generated method stub
+
+}
+private void cambioDeImagenes() {
+	// TODO Auto-generated method stub
+
+}
+private void rotacion() {
+	// TODO Auto-generated method stub
+
+}
+}
+ 
