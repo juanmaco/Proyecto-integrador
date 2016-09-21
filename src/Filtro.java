7e118cc6f7cdd8a67a63188edffd933f48dd38ea
Daniel Lince <josedaniellince@hotmail.com>
2016-09-21 09:56:59 -0500

    Se creo un metodo estatico que comprobara si los archivos estan dentro de un directorio y que extension contiene

--- src/Filtro.java
+++ src/Filtro.java
@@ -7,25 +7,35 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class Filtro {
-	
-	//Archivo que representa la carpeta donde se usara el FileChooser
+
+	// Archivo que representa la carpeta donde se usara el FileChooser
 	static final File dir = new File("../data");
-	//Se crea una lista que contendra las imagenes
-	static final String[] EXTENSIONS = new String[]{
-	"png", "jpg"
-};
-	//filtro para identificar la extension de las imagenes
-	static final Filenamefilter IMAGE_FILTER = new FilenameFilter(){
-		
-		public boolean accept(final File dir, final String name){
-			for(final String ext: EXTENSIONS){
-				if(name.endsWith("."+ext)){
-					return(true);
+	// Se crea una lista que contendra las imagenes
+	static final String[] EXTENSIONS = new String[] { "png", "jpg" };
+	// filtro para identificar la extension de las imagenes
+	static final FilenameFilter IMAGE_FILTER = new FilenameFilter() {
+
+		public boolean accept(final File dir, final String name) {
+			for (final String ext : EXTENSIONS) {
+				if (name.endsWith("." + ext)) {
+					return (true);
 				}
 			}
-			return(false);
+			return (false);
 		}
-		
-	
+
 	};
+
+	public static void main(String[] args) {
+		if (dir.isDirectory()) {
+			for (final File f : dir.listFiles(IMAGE_FILTER)) {
+				BufferedImage img = null;
+				try {
+					img = ImageIO.read(f);
+				} catch (final IOException e) {
+
+				}
+			}
+		}
+	}
 }
